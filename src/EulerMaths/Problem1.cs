4fd770169b878771a2c33a81bbc62a1292e23f2d
Doron Epstein <doron.epstein@derivco.co.za>
2018-11-02 09:23:11 +0200

    chore: cleanup comments

--- src/EulerMaths/Problem1.cs
+++ src/EulerMaths/Problem1.cs
@@ -5,9 +5,6 @@ namespace EulerMaths
   /// <summary>
   /// Multiples of 3 and 5
   /// <para>https://projecteuler.net/problem=1</para>
-  /// <para>If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.</para>
-  /// <para></para>
-  /// <para>Find the sum of all the multiples of 3 or 5 below 1000.</para>
   /// </summary>
   public static class Problem1
   {
