da831f48c0f7310579206e93819b96efab2d3d3e
Doron Epstein <jimbeanzn@gmail.com>
2018-11-02 17:01:56 +0200

    refactor: problem 2
    ensure we can run the sequence over an infinite amount or at-least till long gets filled

--- src/EulerMaths/Problem2.cs
+++ src/EulerMaths/Problem2.cs
@@ -12,18 +12,28 @@ namespace EulerMaths
   {
     private const string Problem = "Problem 2";
     private const string Title = "Even Fibonacci numbers";
-    private static readonly double Phi = (1 + Math.Sqrt(5)) / 2;
-
-    private static long Fib(int index)
+    
+    private static IEnumerable<long> GetFibonacciSequence()
     {
-      var asymp = Math.Pow(Phi, index) / Math.Sqrt(5);
+      var first = 0L;
+      var second = 1L;
 
-      return Convert.ToInt64(Math.Round(asymp));
+      yield return first;
+      yield return second;
+
+      while (true)
+      {
+        var current = first + second;
+        yield return current;
+
+        first = second;
+        second = current;
+      }
     }
 
     private static long Answer()
     {
-      return Enumerable.Range(0, 50).Select(Fib).Where(n => n < 4000000 && n % 2 == 0).Sum();
+      return GetFibonacciSequence().TakeWhile(n => n < 4000000).Where(n => n % 2 == 0).Sum();
     }
 
     public static string GetAnswer()
