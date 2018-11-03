1b3655fc594da863745a5ac5543c79539506c685
Doron Epstein <jimbeanzn@gmail.com>
2018-11-03 16:40:35 +0200

    feat: base problem 3

---
+++ src/EulerMaths/Problem3.cs
@@ -0,0 +1,24 @@
+﻿using EulerMaths.Helpers;
+
+namespace EulerMaths
+{
+  /// <summary>
+  /// Largest prime factor
+  /// <para>https://projecteuler.net/problem=3</para>
+  /// </summary>
+  internal class Problem3 : ProblemBase
+  {
+    public Problem3()
+    {
+      Problem = "Problem 3";
+      Title = "Largest prime factor";
+    }
+    
+    protected internal override long Answer()
+    {
+      var primes = Primes.Take(20);
+
+      return 0;
+    }
+  }
+}
--- src/EulerMaths/Program.cs
+++ src/EulerMaths/Program.cs
@@ -8,6 +8,7 @@ namespace EulerMaths
     {
       Console.WriteLine(new Problem1().GetAnswer());
       Console.WriteLine(new Problem2().GetAnswer());
+      Console.WriteLine(new Problem3().GetAnswer());
       Console.ReadLine();
     }
   }
