13fe79b16807a19b97cc952e256edfdab23befb6
Doron Epstein <jimbeanzn@gmail.com>
2018-11-08 23:53:44 +0200

    refactor: problem 8

--- src/EulerMaths/Problem8.cs
+++ src/EulerMaths/Problem8.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using EulerMaths.Helpers;
 
 namespace EulerMaths
@@ -7,7 +11,7 @@ namespace EulerMaths
   ///   Largest product in a series
   ///   <para>https://projecteuler.net/problem=8</para>
   /// </summary>
-  internal class Problem8 : ProblemBase<long>
+  internal class Problem8 : ProblemBase<(int[] numbers, int product)>
   {
     public Problem8()
     {
@@ -15,9 +19,57 @@ namespace EulerMaths
       Title = "Largest product in a series";
     }
 
-    protected internal override long Answer()
+    public override string GetAnswer()
     {
-      return 0;
+      var watch = Stopwatch.StartNew();
+      var answer = Answer();
+      watch.Stop();
+
+      return
+        $"{Problem} - {Title}: {answer.product} ({answer.numbers.Aggregate("", (current, number) => current + (number + " * "))}). Execution time in ms: {watch.ElapsedMilliseconds}";
+    }
+
+    protected internal override (int[] numbers, int product) Answer()
+    {
+      const string thousandDigitNumber = "73167176531330624919225119674426574742355349194934" +
+                                         "96983520312774506326239578318016984801869478851843" +
+                                         "85861560789112949495459501737958331952853208805511" +
+                                         "12540698747158523863050715693290963295227443043557" +
+                                         "66896648950445244523161731856403098711121722383113" +
+                                         "62229893423380308135336276614282806444486645238749" +
+                                         "30358907296290491560440772390713810515859307960866" +
+                                         "70172427121883998797908792274921901699720888093776" +
+                                         "65727333001053367881220235421809751254540594752243" +
+                                         "52584907711670556013604839586446706324415722155397" +
+                                         "53697817977846174064955149290862569321978468622482" +
+                                         "83972241375657056057490261407972968652414535100474" +
+                                         "82166370484403199890008895243450658541227588666881" +
+                                         "16427171479924442928230863465674813919123162824586" +
+                                         "17866458359124566529476545682848912883142607690042" +
+                                         "24219022671055626321111109370544217506941658960408" +
+                                         "07198403850962455444362981230987879927244284909188" +
+                                         "84580156166097919133875499200524063689912560717606" +
+                                         "05886116467109405077541002256983155200055935729725" +
+                                         "71636269561882670428252483600823257530420752963450";
+      var thirteenDigitCombinations = new List<int[]>();
+
+      for (var i = 0; i < thousandDigitNumber.Length - 13; i++)
+      {
+        var combination = new int[13];
+        for (var j = 0; j < 13; j++)
+        {
+          combination[j] = Convert.ToInt32(thousandDigitNumber[i + j].ToString());
+        }
+
+        if (combination.All(n => n != 0))
+          thirteenDigitCombinations.Add(combination);
+      }
+
+      return thirteenDigitCombinations
+        .Select(combination => new Tuple<int[], int>(combination, combination.Aggregate(1, (current, i) => current * i)))
+        .OrderByDescending(n => n.Item2)
+        .Select(n => (n.Item1, n.Item2))
+        .First();
     }
   }
 }
