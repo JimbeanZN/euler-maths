1f1c97a96079489be69d6587a653701645ea7a3b
Doron Epstein <jimbeanzn@gmail.com>
2018-11-29 22:50:28 +0200

    chore: code cleanup

--- src/EulerMaths/Problem18.cs
+++ src/EulerMaths/Problem18.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace EulerMaths
@@ -10,64 +9,8 @@ namespace EulerMaths
   /// </summary>
   internal class Problem18 : ProblemBase<long>
   {
-    public Problem18()
+    private static readonly string[] Triangle =
     {
-      Problem = "Problem 18";
-      Title = "Maximum path sum I";
-    }
-
-    protected internal override long Answer()
-    {
-      var inputTriangle = new int[Triangle.Length, Triangle.Length];
-      
-      for (var i = 0; i < Triangle.Length; i++)
-      {
-        var ints = Triangle[i].Split(' ').Select(n => Convert.ToInt32(n.ToString())).ToList();
-        for (var j = 0; j < ints.Count; j++)
-        {
-          inputTriangle[i, j] = ints[j];
-        }
-      }
-
-      for (var i = inputTriangle.GetLength(0) - 2; i >= 0; i--) {
-        for (var j = 0; j <= i; j++) {
-          inputTriangle[i, j] += Math.Max(inputTriangle[i+1,j], inputTriangle[i+1, j+1]);
-        }
-      }
-
-      return inputTriangle[0, 0];
-
-      //foreach (var s in Triangle.Reverse())
-      //{
-      //  var ints = s.Split(' ').Select(n => Convert.ToInt32(n)).ToList();
-
-      //  var x = ints[index];
-
-      //  if (ints.Count == 2)
-      //  {
-      //    x = ints.Max();
-      //  }
-
-      //  if (ints.Count > 2)
-      //  {
-      //    if (index == ints.Count - 1 && ints[index - 1] > x)
-      //    {
-      //      x = ints[index - 1];
-      //    }
-      //    else if (ints[index+1] > x)
-      //    {
-      //      x = ints[index + 1];
-      //    }
-      //  }
-        
-      //  answer += x;
-      //  index = ints.IndexOf(x);
-      //}
-
-      //return answer;
-    }
-
-    private static readonly string[] Triangle = {
       "75",
       "95 64",
       "17 47 82",
@@ -84,5 +27,28 @@ namespace EulerMaths
       "63 66 04 68 89 53 67 30 73 16 69 87 40 31",
       "04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"
     };
+
+    public Problem18()
+    {
+      Problem = "Problem 18";
+      Title = "Maximum path sum I";
+    }
+
+    protected internal override long Answer()
+    {
+      var inputTriangle = new int[Triangle.Length, Triangle.Length];
+
+      for (var i = 0; i < Triangle.Length; i++)
+      {
+        var ints = Triangle[i].Split(' ').Select(n => Convert.ToInt32(n.ToString())).ToList();
+        for (var j = 0; j < ints.Count; j++) inputTriangle[i, j] = ints[j];
+      }
+
+      for (var i = inputTriangle.GetLength(0) - 2; i >= 0; i--)
+      for (var j = 0; j <= i; j++)
+        inputTriangle[i, j] += Math.Max(inputTriangle[i + 1, j], inputTriangle[i + 1, j + 1]);
+
+      return inputTriangle[0, 0];
+    }
   }
 }
