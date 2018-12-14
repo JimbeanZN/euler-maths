5d48ec70dbc404fc55164b40294ee97cb8a42827
Doron Epstein <doron.epstein@derivco.co.za>
2018-12-14 10:43:26 +0200

    feat: solve problem 23

---
+++ src/EulerMaths/1-100/20-29/Problem23.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EulerMaths
+{
+  /// <summary>
+  ///   Non-abundant sums
+  ///   <para>https://projecteuler.net/problem=23</para>
+  /// </summary>
+  internal class Problem23 : ProblemBase<long>
+  {
+    public Problem23()
+    {
+      Problem = "Problem 23";
+      Title = "Non-abundant sums";
+    }
+
+    protected internal override long Answer()
+    {
+      var baseNumbers = Enumerable.Range(1, 28123).ToList();
+      var abundantNumbers = baseNumbers.AsParallel().Where(IsAbundantNumber).ToList();
+      var abundantSums = new List<int>();
+
+      for (var i = 0; i < abundantNumbers.Count(); i++)
+      {
+        for (var j = i; j < abundantNumbers.Count(); j++)
+        {
+          abundantSums.Add(abundantNumbers[i] + abundantNumbers[j]);
+        }
+      }
+
+      return baseNumbers.Except(abundantSums).Sum();
+    }
+
+    private static bool IsAbundantNumber(int num)
+    {
+      return Enumerable.Range(1, num - 1).Where(n => num % n == 0).Sum() > num;
+    }
+  }
+}
